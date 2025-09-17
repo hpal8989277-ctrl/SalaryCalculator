23b7759b07bc3f57ae3b854b16dc9c4130f4fb76
Himanshu <hpal8989277@gmail.com>
2025-09-17 16:46:36 +0530

    my first commit

---
+++ src/SalaryCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SalaryCalculator {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+        System.out.print("enter your salary : ");
+        double salary = sc.nextDouble();
+
+        System.out.print("enter your interest : ");
+        double interest = sc.nextDouble();
+
+        double tax =  salary * interest / 100;
+
+        System.out.println("your Tax on "+ salary + " salary is : "+ tax);
+        
+        double remainingSalary = salary - tax;
+        System.out.println("your remaining salary(after tax) is : "+ remainingSalary);
+
+        sc.close();
+    }
+}
